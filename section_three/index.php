1f5b3f172a22b616b0d385ea87d4dd265fa0db54
patricknietzschmann <patricknietzschmann@localhost>
2018-10-25 11:42:42 +0200

    section 3 nochmal, diesmal mit den Programmierrichtlinien (vermutlich glaube ich ggf.)

--- section_three/index.php
+++ section_three/index.php
@@ -8,12 +8,16 @@ $value = "Keine Angabe";
 //find Orientation
 $template = $dataSets[0];
 $template = explode(";", $template);
-for ($i=0;$i<count($template);$i++){
-    if ($template[$i] == $key){
+for ($i=0;$i<count($template);$i++)
+{
+    if ($template[$i] == $key)
+    {
         //suchattribut belegen
         $key = $i;
         break;
-    }elseif ($i == (count($template)-1)){
+    }
+    elseif ($i == (count($template)-1))
+    {
         die("das Atribut ist nicht enthalten.");
     }
 }
@@ -21,29 +25,39 @@ echo "Das gesuchte Atribut befindet sich an Stelle ".$key." eines Datensatzes<br
 //finden und Sammeln aller zutreffenden Datensätze
 $dataSetPositives = array(); //das Array, das alle zutreffenden Datensätze als referenz aufnimmt
 $dataSetValuePosibilities = array(); //FTLOLZ alle Möglichen Values für $key 
-for ($i=1;$i<count($dataSets);$i++){
-    if (explode(";", $dataSets[$i])[$key]==$value){
+for ($i=1;$i<count($dataSets);$i++)
+{
+    if (explode(";", $dataSets[$i])[$key]==$value)
+    {
         $dataSetPositives[] = $i;
-        //echo $i."; ".$dataSets[$i]."<br><br>";
-    }else{ //FTLolz (kann weg)
-        if (!in_array(explode(";", $dataSets[$i])[$key], $dataSetValuePosibilities)){
+        //echo $i."; ".$dataSets[$i]."<br><br>"; //kontrolle falls ausgaben nicht korrekt sind
+    }
+    else //FTLolz (kann weg)
+    {
+        if (!in_array(explode(";", $dataSets[$i])[$key], $dataSetValuePosibilities))
+        {
             $dataSetValuePosibilities[] = explode(";", $dataSets[$i])[$key];
         }
     }
 }
+
 //Ausgabe
     //Formatierung
 $outStringPositives = "wurde";
-if (count($dataSetPositives)==1){
+if (count($dataSetPositives)==1)
+{
     $outStringPositives .= " ein Datensatz";
-}else{
+}
+else
+{
     $outStringPositives .= "n ".count($dataSetPositives)." Datensätze";
 }
     //Print
 echo "Es ".$outStringPositives." mit dem Wert: \""
     .$value."\" für das Atribut \"".$keyName
     ."\" gefunden.<br>Andere mögliche Werte, die man für dieses Atribut auszählen kann sind:<br>";
-for ($i=0;$i<count($dataSetValuePosibilities);$i++){
+for ($i=0;$i<count($dataSetValuePosibilities);$i++)
+{
     echo "   -".$dataSetValuePosibilities[$i]."<br>";
 }
 
